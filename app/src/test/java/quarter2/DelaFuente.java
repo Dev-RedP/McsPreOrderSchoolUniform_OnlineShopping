651deb989550a47b1fbbc56b0d34e8980418ddaa
Benjamin-432 <21-000253@mcstayuman.edu.ph>
2026-09-18 11:33:23 +0800

    added DelaFuente

--- app/src/test/java/quarter2/DelaFuente.java
+++ app/src/test/java/quarter2/DelaFuente.java
@@ -1,4 +1,18 @@
-package quarter2;
+package com.example.mcsonlineschooluniform_preorder.quarter2;
+
+import org.junit.Test;
 
 public class DelaFuente {
+    @Test
+    public void printMyProfile() {
+        String myName = "DELAFUENTE";
+        String petName = "PICKLES";
+        String favFood = "GINILING";
+        int myAge = 17;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
 }
