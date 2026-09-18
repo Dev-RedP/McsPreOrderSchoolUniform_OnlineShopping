6486047472729b5fa9aac992c183a3fca0729e54
Dev-RedP <redparungaowtf@gmail.com>
2026-09-18 11:27:26 +0800

    done

---
+++ app/src/test/java/quarter2/Parungao.java
@@ -0,0 +1,21 @@
+package quarter2;
+
+import org.junit.Test;
+
+public class Parungao {
+    @Test
+    public void printMyprofile() {
+        String myName = "Parungao";
+        String petname1 = "nami";
+        String petname2 = "kimchi";
+        String favfood = "kare-kare";
+        int myAge = 17;
+
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is" + myName + " and I am + myAge +");
+        System.out.println("I have 2 wonderful pets named" + petname1 + " and " + petname2 + ".");
+        System.out.println("If I could eat " + favfood + " everyday!");
+
+    }
+}
